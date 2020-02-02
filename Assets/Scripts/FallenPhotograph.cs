c9a4b35be9640eb4a78f1ef7c6523ce20ac9121b
Steven Messner <stevenjmessner@gmail.com>
2020-02-02 01:05:30 -0700

    Adds scenes for two rooms
    Adds scenes for robot room, bedroom, and associated assets

---
+++ Assets/Scripts/FallenPhotograph.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FallenPhotograph : MonoBehaviour
+{
+    private bool photographIsFallen = false;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        if (photographIsFallen == true)
+        {
+            GetComponent<SpriteRenderer>().enabled = true;
+        }
+        else
+        {
+            gameObject.SetActive(false);
+        }
+    }
+
+    public void MakePhotographFall()
+    {
+        photographIsFallen = true;
+    }
+}
