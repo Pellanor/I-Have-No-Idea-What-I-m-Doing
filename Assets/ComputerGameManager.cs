39537b41fee512ee1413e77e7d52ffd94cea9bf2
Steven Messner <stevenjmessner@gmail.com>
2020-02-02 14:49:28 -0700

    Added computer terminal

---
+++ Assets/ComputerGameManager.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class ComputerGameManager : MonoBehaviour
+{
+    public AudioClip mouseClick;
+    public GameObject loginScreen;
+    public GameObject menuScreen;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetMouseButtonDown(0))
+        {
+            GetComponent<AudioSource>().PlayOneShot(mouseClick);
+        }
+    }
+
+    public void Login()
+    {
+        loginScreen.SetActive(false);
+        menuScreen.SetActive(true);
+    }
+
+    public void ExitComputer()
+    {
+        SceneManager.LoadScene("Generator Room");
+    }
+
+    public void OpenLocker()
+    {
+        
+    }
+
+}
---
+++ Assets/Scripts/MusicController.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MusicController : MonoBehaviour
+{
+    private static MusicController _instance;
+
+    public static MusicController Instance { get { return _instance; } }
+
+    private void Awake()
+    {
+        if (_instance != null && _instance != this)
+        {
+            Destroy(this.gameObject);
+        }
+        else
+        {
+            _instance = this;
+            DontDestroyOnLoad(this);
+        }
+    }
+}
+
+
