f042ed5d1450771ceacbf25722a9645b53e4f546
Pellanor <pellanor@gmail.com>
2020-02-02 01:32:13 -0700

    Sync again

---
+++ Assets/Inventory.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Inventory : MonoBehaviour
+{
+    public GameObject CurrentlySelected;
+
+    public GameObject VoiceChip;
+    public GameObject USBDrive;
+    public GameObject Arm1;
+    public GameObject Arm2;
+    public GameObject OpticalSensorArray;
+    public GameObject PowerCell;
+
+    public void SelectVoiceChip()
+    {
+        this.CurrentlySelected = VoiceChip;
+    }
+    public void SelectUSBDrive()
+    {
+        this.CurrentlySelected = USBDrive;
+    }
+    public void SelectArm1()
+    {
+        this.CurrentlySelected = Arm1;
+    }
+    public void SelectArm2()
+    {
+        this.CurrentlySelected = Arm2;
+    }
+    public void SelectOpticalSensorArray()
+    {
+        this.CurrentlySelected = OpticalSensorArray;
+    }
+    public void SelectPowerCell()
+    {
+        this.CurrentlySelected = PowerCell;
+    }
+
+    public GameObject GetCurrentlySelected()
+    {
+        return this.CurrentlySelected;
+    }
+}
---
+++ Assets/Receiver.cs
@@ -0,0 +1,62 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class Receiver : MonoBehaviour,
+                        IPointerDownHandler,
+                        IPointerEnterHandler,
+                        IPointerExitHandler
+{
+    public Inventory Inventory;
+
+    /**
+      * Handle mouse moving onto the robot part.
+      * This sets the cursor.
+      */
+    public void OnPointerEnter(PointerEventData eventData)
+    {
+        Debug.Log("Cursor entering: " + eventData.pointerCurrentRaycast.gameObject.name);
+        // if(Enterable)
+        // {
+        //     Cursor.SetCursor(EnterCursorSprite, EnterCursorOffset, CursorMode.Auto);
+        // }
+        // else
+        // {
+        //     Cursor.SetCursor(NoEntryCursorSprite, NoEntryCursorOffset, CursorMode.Auto);
+        // }
+    }
+
+    /**
+      * Handles mouse moving off the robot part.
+      * This resets the cursor.
+      */
+    public void OnPointerExit(PointerEventData eventData)
+    {
+        Debug.Log("Cursor exiting: " + eventData);
+        ResetCursor();
+    }
+
+    /** 
+      * Handles clicking on this robot piece.
+      * If it can be collected into inventory, it sets inventory as destination.
+      */
+    public void OnPointerDown(PointerEventData eventData)
+    {
+        Debug.Log("Clicked: " + eventData.pointerCurrentRaycast.gameObject.name);
+
+        GameObject selected = this.Inventory.GetCurrentlySelected();
+        RobotPartHandler robotPart = selected.GetComponent<RobotPartHandler>();
+
+        if(selected != null && robotPart != null)
+        {
+            robotPart.Attach();
+            ResetCursor();
+        }
+    }
+
+    private void ResetCursor()
+    {
+        Cursor.SetCursor(null, Vector2.zero, CursorMode.Auto);
+    }
+}
