3be4e0167b18cbf35ee4de2c9cdcf226b5f21c4f
Pellanor <pellanor@gmail.com>
2020-02-02 17:03:25 -0700

    more stuff

---
+++ Assets/TitleScreen.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class TitleScreen : MonoBehaviour
+{
+    public void Go() {
+        SceneManager.LoadScene("Bedroom");
+    }
+
+    public void Exit() {
+        Application.Quit();
+    }
+}
